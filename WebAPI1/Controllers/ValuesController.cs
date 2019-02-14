f660b550a16d0ea407119c54658b93855ce12a84
Alexander Kotkevych <kotki89@gmail.com>
2019-02-14 23:45:04 +0200

    initial commit

--- WebAPI1/Controllers/ValuesController.cs
+++ WebAPI1/Controllers/ValuesController.cs
@@ -1,4 +1,7 @@
-﻿using System.Linq;
+﻿using System;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using StackExchange.Redis;
@@ -10,46 +13,70 @@ namespace WebAPI1.Controllers
     public class ValuesController : ControllerBase
     {
         private readonly IConfiguration _config;
-        private readonly DockerTestContext _context;
+        //private readonly DockerTestContext _context;
+        private readonly IHttpClientFactory _clientFactory;
 
         private readonly string _redisKey = "SomeRandomRedisKey";
 
-        public ValuesController(IConfiguration config, DockerTestContext context)
+        public ValuesController(IConfiguration config,
+                                //DockerTestContext context,
+                                IHttpClientFactory clientFactory)
         {
             _config = config;
-            _context = context;
+            //_context = context;
+            _clientFactory = clientFactory;
         }
 
         [HttpGet]
-        public ActionResult<string> Get()
+        public async Task<ActionResult<string>> Get()
         {
-            var rcString = _config.GetValue<string>("REDIS_URL");
-            //var rcString = "localhost:6379";
+            string result = null;
 
-            string result = string.Empty;
+            var client = _clientFactory.CreateClient();
 
-            ConnectionMultiplexer redis = ConnectionMultiplexer.Connect(rcString);
-            IDatabase db = redis.GetDatabase();
+            var request = new HttpRequestMessage(HttpMethod.Get,
+                "http://localhost:51442/api/values");
 
-            var cache = db.StringGet(_redisKey);
+            var response = await client.SendAsync(request);
 
-            if (cache.IsNullOrEmpty)
+            if (response.IsSuccessStatusCode)
             {
-                //change to aggregate
-                var dbStrings = _context.DockerTestTable.Select(dtt => dtt.ToString());
-                foreach(var s in dbStrings)
-                {
-                    result += result + s;
-                }
-
-                db.StringSet(_redisKey, result);
+                result = await response.Content.ReadAsStringAsync();
             }
             else
             {
-                result = cache.ToString() + " from REDIS!";;
+                result = "Something vent wrong with response!";
             }
 
             return result;
+
+            //var rcString = _config.GetValue<string>("REDIS_URL");
+            ////var rcString = "localhost:6379";
+
+            //string result = string.Empty;
+
+            //ConnectionMultiplexer redis = ConnectionMultiplexer.Connect(rcString);
+            //IDatabase db = redis.GetDatabase();
+
+            //var cache = db.StringGet(_redisKey);
+
+            //if (cache.IsNullOrEmpty)
+            //{
+            //    //change to aggregate
+            //    var dbStrings = _context.DockerTestTable.Select(dtt => dtt.ToString());
+            //    foreach(var s in dbStrings)
+            //    {
+            //        result += s;
+            //    }
+
+            //    db.StringSet(_redisKey, result);
+            //}
+            //else
+            //{
+            //    result = cache.ToString() + " from REDIS!";;
+            //}
+
+            //return result;
         }
     }
 }
--- WebAPI2/Controllers/ValuesController.cs
+++ WebAPI2/Controllers/ValuesController.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net.Http;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 
 namespace WebAPI2.Controllers
 {
@@ -11,20 +6,10 @@ namespace WebAPI2.Controllers
     [ApiController]
     public class ValuesController : ControllerBase
     {
-        private readonly IHttpClientFactory _httpClientFactory;
-
-        public ValuesController(/*IHttpClientFactory httpClientFactory*/)
-        {
-            //_httpClientFactory = httpClientFactory;
-            //_httpClientFactory = new HttpClientFactory();
-        }
-
         [HttpGet]
         public ActionResult<string> Get()
         {
-            //var client = HttpClientFactory.Create()
-
-            return string.Empty;
+            return "Hello from WebAPI2";
         }
     }
 }
